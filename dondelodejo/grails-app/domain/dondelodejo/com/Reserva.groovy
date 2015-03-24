c446a6625ebc0857dda79ab704db6c1fca137b60
phakuphaku <phakuphaku@hotmail.com>
2015-03-24 18:19:51 -0300

    nuevo metodo, en reserva arreglo la calififacionNueva a calificacion

--- dondelodejo/grails-app/domain/dondelodejo/com/Reserva.groovy
+++ dondelodejo/grails-app/domain/dondelodejo/com/Reserva.groovy
@@ -20,7 +20,7 @@ class Reserva {
 	static constraints = {
 		estado (nullable:true,inList:[
 			"Cargada",
-			"Aceptada",
+			"Aceptada","Calificada",
 			"Cancelada"
 		])
 		calificacion nullable:true
@@ -40,7 +40,16 @@ class Reserva {
 		}
 		return results
 	}
-
+	void calificar (Usuario user, int valor, char detalle) {
+		
+		 Calificacion calificacion = new Calificacion(["valor":valor, "detalle":detalle])
+		 this.calificacion = calificacion
+		 int cantidad = Reserva.findByEstacionamientoAndEstado(this.estacionamiento.id,"Calificada").size()+1
+		 int puntaje = this.estacionamiento.puntaje
+		 def nuevoPuntaje = (puntaje + valor)/cantidad
+		 this.estacionamiento.puntaje = nuevoPuntaje
+		 
+		 }
 }
 class Calificacion {
 	
