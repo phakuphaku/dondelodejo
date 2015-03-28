7ffabb76a8071ea342394038ff9fa68c58c04e8e
Lothcaramon <lothcaramon@gmail.com>
2015-03-28 18:51:54 -0300

    Reserva service y esas cosas

---
+++ dondelodejo/test/unit/dondelodejo/ReservaServiceSpec.groovy
@@ -0,0 +1,21 @@
+package dondelodejo
+
+import dondelodejo.com.ReservaService;
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ReservaService)
+class ReservaServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
